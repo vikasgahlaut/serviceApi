809f775221e0e3202838e66f488c852c785ff1ba
Vikas Gahlaut <gahlaut@adobe.com>
2022-02-24 01:28:42 +0530

    Fix logger

--- controller/projectController.rb
+++ controller/projectController.rb
@@ -4,10 +4,10 @@ require 'uuid'
 require 'logger'
 
 # Create a Logger that prints to STDOUT
-log = Logger.new(STDOUT)
+@log = Logger.new(STDOUT)
 
 def createProject(body)
-  log.info(I am inside fucking createProject)
+  log.info(I am inside fucking createProjects)
   jsonBody = JSON.parse(body) 
   client = Mysql2::Client.new(:host => "192.168.1.134", :port => "3306", :username => "db", :password => "Bajaj@3901")
   id = UUID.new.generate
