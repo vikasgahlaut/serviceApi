8217a5da4f674fbcbcd27c3c906ac151e9d1d994
Vikas Gahlaut <gahlaut@adobe.com>
2022-02-24 01:36:41 +0530

    Fix logger

--- controller/projectController.rb
+++ controller/projectController.rb
@@ -7,13 +7,13 @@ require 'logger'
 $log = Logger.new(STDOUT)
 
 def createProject(body)
-  log.info('I am inside fucking createProjects')
   jsonBody = JSON.parse(body) 
   client = Mysql2::Client.new(:host => "192.168.1.134", :port => "3306", :username => "db", :password => "Bajaj@3901")
   id = UUID.new.generate
   insertQuery = "INSERT INTO cmsDB.PROJECTS (`projectId`, `projectName`, `projectType`, `clientApp`) VALUES ('#{id}', '#{jsonBody['projectName']}', '#{jsonBody['projectType']}', '#{jsonBody['clientApp']}' );"
   client.query(insertQuery)
-  
+  $log.info('Query Executed: ' + insertQuery)
+
   testHash = {
     :id => id,
     :name => "#{jsonBody['projectName']}"
@@ -24,7 +24,9 @@ end
 def getProjects
   client = Mysql2::Client.new(:host => "192.168.1.134", :port => "3306", :username => "db", :password => "Bajaj@3901")
   projects = []
-  client.query("select * from cmsDB.PROJECTS") .each do |row|
+  getsQuery = "select * from cmsDB.PROJECTS"
+  $log.info('Query Executed: ' + getsQuery)
+  client.query(getsQuery) .each do |row|
     projects.push(row)
   end
   return projects.to_json() 
@@ -33,7 +35,7 @@ end
 def getProject(id)
   client = Mysql2::Client.new(:host => "192.168.1.134", :port => "3306", :username => "db", :password => "Bajaj@3901")
   getQuery = "select * from cmsDB.PROJECTS WHERE projectId = '#{id}'"
-  puts getQuery
+  $log.info('Query Executed: ' + getQuery)
   project = []
   client.query(getQuery) .each do |row|
     project.push(row)  
@@ -44,6 +46,7 @@ end
 def deleteProject(id)
   client = Mysql2::Client.new(:host => "192.168.1.134", :port => "3306", :username => "db", :password => "Bajaj@3901")
   deleteQuery = "DELETE FROM cmsDB.PROJECTS WHERE projectId = '#{id}'"
+  $log.info('Query Executed: ' + deleteQuery)
   client.query(deleteQuery)
   return 'Project Deleted Successfully' 
 end
