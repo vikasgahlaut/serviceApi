1b9779d466400cb7f77bb863c4f9907f1018cb2e
Vikas Gahlaut <vikas.gahlaut@gmail.com>
2022-02-28 07:20:15 +0000

    Fix error logs

--- controller/projectController.rb
+++ controller/projectController.rb
@@ -9,7 +9,7 @@ def createProject(body)
   begin
     client = Mysql2::Client.new(:host => "192.168.1.134", :port => "3306", :username => "db", :password => "Bajaj@3901")
     id = UUID.new.generate
-    insertQuery = "INSRT INTO cmsDB.PROJECTS (`projectId`, `projectName`, `projectType`, `clientApp`) VALUES ('#{id}', '#{jsonBody['projectName']}', '#{jsonBody['projectType']}', '#{jsonBody['clientApp']}' );"
+    insertQuery = "INSERT INTO cmsDB.PROJECTS (`projectId`, `projectName`, `projectType`, `clientApp`) VALUES ('#{id}', '#{jsonBody['projectName']}', '#{jsonBody['projectType']}', '#{jsonBody['clientApp']}' );"
     begin  
       client.query(insertQuery)
       $log.info('Query Executed: ' + insertQuery)
