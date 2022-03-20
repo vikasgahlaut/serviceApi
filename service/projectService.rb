b0c380b69170866f17711cf2c2fd3f70833b503c
Vikas Gahlaut <gahlaut@adobe.com>
2022-03-21 00:54:47 +0530

    Comment exceptions code

--- service/projectService.rb
+++ service/projectService.rb
@@ -36,12 +36,13 @@ get '/project/:id',:provides=>:json do
   searchId = param[3,param.length]
   response = getProject(searchId)
   $logs.info('Success response: ' + response);
+  status 200
   #begin
     #response = getProject(searchId)
     #$logs.info('Success response: ' + response);
     #status 200
   #rescue Exception404 => e
-    status 404
+    #status 404
   #end  
   #$log.info('Response: ' + response) 
   #$logs.info('Response: ' + response)
